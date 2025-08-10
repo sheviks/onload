7a90025a579045ae17430b1ed54421d23d505b13
Sheviks Chen <sheviks@gmail.com>
2025-08-10 09:16:37 +0800

    Fix compatibility with Linux kernel 6.15–6.16 ABI/API changes
    - Rename `page->index` to `page->__folio_index`
      [acc53a0](https://github.com/torvalds/linux/commit/acc53a0b4c156877773da6e9eea4113dc7e770ae)
    
    - Replace `d_hash_and_lookup()` with `try_lookup_noperm()`
      [06c5674](https://github.com/torvalds/linux/commit/06c567403ae5a0b56005c2d4a184c903f572c844)
    
    - Fix `EFRM_HAVE_OLD_FAULT`; `vm_operations_struct::fault()` has always been single-parameter, and `EFRM_HAVE_NEW_FAULT` remains valid
      [1c8f422](https://github.com/torvalds/linux/commit/1c8f422059ae5da07db7406ab916203f9417e396)

--- src/driver/linux_resource/kernel_compat.sh
+++ src/driver/linux_resource/kernel_compat.sh
@@ -47,7 +47,7 @@ EFRM_HAVE_ALLOC_FILE_PSEUDO		symbol	alloc_file_pseudo	include/linux/file.h
 EFRM_NET_HAS_PROC_INUM			member	struct_net proc_inum	include/net/net_namespace.h
 EFRM_NET_HAS_USER_NS			member	struct_net user_ns	include/net/net_namespace.h
 
-EFRM_HAVE_OLD_FAULT			memtype struct_vm_operations_struct	fault	include/linux/mm.h	int (*)(struct vm_area_struct *vma, struct vm_fault *vmf)
+EFRM_HAVE_OLD_FAULT			memtype struct_vm_operations_struct	fault	include/linux/mm.h	int (*)(struct vm_fault *vmf)
 EFRM_HAVE_NEW_FAULT			memtype struct_vm_operations_struct	fault	include/linux/mm.h	vm_fault_t (*)(struct vm_fault *vmf)
 
 EFRM_HAVE_SCHED_TASK_H			file	include/linux/sched/task.h
@@ -172,6 +172,8 @@ EFRM_F_OWNER_IS_VAL		memtype	struct_file	f_owner	include/linux/fs.h struct fown_
 EFRM_CLOEXEC_FILES_STRUCT	symtype	close_on_exec	include/linux/fdtable.h	bool(unsigned int, const struct files_struct *)
 
 EFRM_HAVE_GET_FILE_RCU_FUNC symtype get_file_rcu include/linux/fs.h struct file*(struct file __rcu **)
+
+EFRM_PAGE_HAS_FOLIO_INDEX		member	struct_page	__folio_index	include/linux/mm_types.h
 
 # TODO move onload-related stuff from net kernel_compat
 " | grep -E -v -e '^#' -e '^$' | sed 's/[ \t][ \t]*/:/g'
